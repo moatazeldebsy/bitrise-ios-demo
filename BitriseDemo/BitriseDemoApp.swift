88baad23f811f2090ca51ce00cebf1f434c13a28
Moataz Nabil <moataz.eldebsy@moataznabil-mbp.localdomain>
2021-07-02 01:34:14 +0200

    add first commit

---
+++ BitriseDemo/BitriseDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  BitriseDemoApp.swift
+//  BitriseDemo
+//
+//  Created by Moataz Nabil on 01.07.21.
+//
+
+import SwiftUI
+
+@main
+struct BitriseDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
